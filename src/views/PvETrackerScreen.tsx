5cf287ddfc3acb2c1d2404756ce5dedf8b95f0fc
0Nom4D <arthur.adam@epitech.eu>
2022-06-09 23:51:50 +0200

    GuardianDock: custom_autocomplete - Adding first base for AutoComplete Native Base Component

--- src/views/PvETrackerScreen.tsx
+++ src/views/PvETrackerScreen.tsx
@@ -1,59 +1,64 @@
 import React, {useEffect, useState} from "react";
 
-import {Divider, Input, View, VStack} from "native-base";
+import {Box, FlatList, Input, Pressable, Text, View, VStack} from "native-base";
 import {ImageBackground} from "react-native";
 
-import {parseDisplayName} from "../utils/utils";
-
-import {API_ROUTE, BUNGIE_API_KEY} from "@env";
 import CenteredAlert from "../components/CenteredAlert";
+import {fetchUsers} from "../utils/BungieAPI";
 
-const fetchUsers = async (id: string) : Promise<any> => {
-    const [displayName, displayNameCode] = parseDisplayName(id)
-
-    if (displayName === undefined || displayNameCode !== undefined)
-        return {};
-    const response = await fetch(API_ROUTE + '/User/Search/GlobalName/0/', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            'X-API-KEY': BUNGIE_API_KEY
-        },
-        body: JSON.stringify({ "displayNamePrefix": displayName })
-    });
-
-    if (response.ok)
-        return response.json()
-    console.error('An error occurred when trying to fetch users.')
-    return null
-}
+import {SerializeUserInfo} from '../utils/utils';
+import {UserInfoCard} from "../types/UserInfoCard.type";
 
 export default function PvETrackerScreen() {
-    const [bungieId, setBungieId] = useState<string>('')
-    const [isError, setError] = useState<boolean>(false)
+    const [userList, setUserList] = useState<Array<UserInfoCard>>([]);
+    const [isError, setError] = useState<boolean>(false);
+    const [bungieId, setBungieId] = useState<string>('');
+
+    const [test, setTest] = useState<string>('');
+
+    const [isFocused, setInputFocus] = useState<boolean>(false);
 
     useEffect(() => {
-        console.log(bungieId);
+        let usersList: Array<UserInfoCard> = [];
+
         fetchUsers(bungieId).then((data) => {
             if (data === null)
                 setError(true);
-            console.log(data.Response.searchResults);
-        })
+            for (const userInfo of data.Response.searchResults)
+                usersList = [...usersList, SerializeUserInfo(userInfo)];
+            setUserList(usersList);
+        });
     }, [bungieId]);
 
     return <>
         <ImageBackground source={require('../../assets/D2Background.jpg')} style={{ width: '100%', height: '100%' }}>
             <View flex='1' alignItems='center'>
-                <VStack safeAreaTop w='90%' space='5' alignItems='center' divider={<Divider />} >
+                <VStack safeAreaTop w='90%' alignItems='center'>
                     <Input
                         variant='underlined'
                         w='85%'
                         size='md'
+                        onFocus={() => setInputFocus(true)}
+                        onBlur={() => setInputFocus(false)}
                         style={{ color: '#ffffff' }}
                         placeholder='Guardian Display Name'
                         onChangeText={(guardianDisplayName: string) => setBungieId(guardianDisplayName)}
                     />
+                    { !test ? <FlatList
+                        w='85%'
+                        data={userList}
+                        renderItem={({item}) => (
+                            <Pressable onPress={() => setTest('ALED')} backgroundColor='#ffffff' borderBottomWidth='1' _dark={{borderColor: 'coolGray.200'}} pl='4' pr='5' py='2'>
+                                {/*<HStack space={3} justifyContent='space-between'>*/}
+                                    <Text _dark={{color: 'warmGray.200'}} bold>
+                                        {item.bungieGlobalDisplayName + '#' + item.bungieGlobalDisplayNameCode}
+                                    </Text>
+                                {/*</HStack>*/}
+                            </Pressable>
+                            )
+                        }
+                        keyExtractor={(_, index) => index.toString()}
+                    /> : null }
                 </VStack>
             </View>
             {isError ?
