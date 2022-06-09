716916a94c0a38f555f1543a27cb491c5b3f8c91
0Nom4D <arthur.adam@epitech.eu>
2022-06-09 23:51:09 +0200

    GuardianDock: custom_autocomplete - Adding Bungie API Request and Serialize Method in utils

---
+++ src/utils/BungieAPI.ts
@@ -0,0 +1,23 @@
+import {parseDisplayName} from "./utils";
+import {API_ROUTE, BUNGIE_API_KEY} from "@env";
+
+export const fetchUsers = async (id: string) : Promise<any> => {
+    const [displayName, displayNameCode] = parseDisplayName(id)
+
+    if (displayName === undefined || displayNameCode !== undefined)
+        return {};
+    const response = await fetch(API_ROUTE + '/User/Search/GlobalName/0/', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            'X-API-KEY': BUNGIE_API_KEY
+        },
+        body: JSON.stringify({ "displayNamePrefix": displayName })
+    });
+
+    if (response.ok)
+        return response.json()
+    console.error('An error occurred when trying to fetch users.')
+    return null
+};
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,30 @@
+import {DestinyMembershipInfo, UserInfoCard} from "../types/UserInfoCard.type";
+import { DestinyMembership } from "../types/DestinyMembership.enum";
+
 export const parseDisplayName = (displayName: string): [string, string] => {
     const parsedName = displayName.split('#')
     return [parsedName[0], parsedName[1]]
+}
+
+export const SerializeUserInfo = (data: any): UserInfoCard => {
+    let membershipInfoArray: Array<DestinyMembershipInfo> = [];
+
+    for (const membership of data.destinyMemberships) {
+        const newMembership: DestinyMembershipInfo = {
+            iconPath: membership.iconPath,
+            crossSaveOverride: Object.values(DestinyMembership).indexOf(membership.crossSaveOverride as DestinyMembership),
+            applicableMembershipTypes: membership.applicableMembershipTypes.map((item: number) => Object.values(DestinyMembership).indexOf(item as DestinyMembership)),
+            isPublic: membership.isPublic,
+            membershipType: Object.values(DestinyMembership).indexOf(membership.membershipType as DestinyMembership),
+            displayName: membership.displayName,
+        } as DestinyMembershipInfo;
+        membershipInfoArray = [...membershipInfoArray, newMembership];
+    }
+
+    return {
+        bungieGlobalDisplayName: data.bungieGlobalDisplayName,
+        bungieGlobalDisplayNameCode: data.bungieGlobalDisplayNameCode.toString().padStart(4, '0'),
+        bungieNetMembershipId: data.bungieNetMembershipId,
+        destinyMemberships: membershipInfoArray
+    };
 }
