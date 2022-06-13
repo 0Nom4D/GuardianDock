bb901e3b98d5c0ac0dcdadf4dab3e6a9ab71d748
Nom4D- <yoshi.arthur@laposte.net>
2022-06-13 21:00:12 +0200

    GuardianDock : custom_autocomplete - Removing AutoComplete

--- src/views/PvETrackerScreen.tsx
+++ src/views/PvETrackerScreen.tsx
@@ -1,34 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState} from 'react';
 
-import {Box, FlatList, Input, Pressable, Text, View, VStack} from "native-base";
-import {ImageBackground} from "react-native";
+import {Box, FlatList, HStack, Input, Pressable, Text, View, VStack} from 'native-base';
+import {ImageBackground} from 'react-native';
 
-import CenteredAlert from "../components/CenteredAlert";
-import {fetchUsers} from "../utils/BungieAPI";
+import CenteredAlert from '../components/CenteredAlert';
+import {fetchUsers} from '../utils/BungieAPI';
 
 import {SerializeUserInfo} from '../utils/utils';
-import {UserInfoCard} from "../types/UserInfoCard.type";
+import {UserInfoCard} from '../types/UserInfoCard.type';
 
 export default function PvETrackerScreen() {
     const [userList, setUserList] = useState<Array<UserInfoCard>>([]);
     const [isError, setError] = useState<boolean>(false);
     const [bungieId, setBungieId] = useState<string>('');
 
-    const [test, setTest] = useState<string>('');
+    const [_, setInputFocus] = useState<boolean>(false);
 
-    const [isFocused, setInputFocus] = useState<boolean>(false);
+    const [selectedProfile, setSelectedProfile] = useState<string>('');
 
     useEffect(() => {
         let usersList: Array<UserInfoCard> = [];
 
         fetchUsers(bungieId).then((data) => {
-            if (data === null)
+            if (data.Response.searchResults == undefined)
                 setError(true);
             for (const userInfo of data.Response.searchResults)
                 usersList = [...usersList, SerializeUserInfo(userInfo)];
             setUserList(usersList);
         });
     }, [bungieId]);
+
+    useEffect(() => {
+        let pveStats: string;
+
+        if (selectedProfile === '')
+            return;
+    }, [selectedProfile]);
 
     return <>
         <ImageBackground source={require('../../assets/D2Background.jpg')} style={{ width: '100%', height: '100%' }}>
@@ -38,27 +45,17 @@ export default function PvETrackerScreen() {
                         variant='underlined'
                         w='85%'
                         size='md'
+                        value={bungieId}
                         onFocus={() => setInputFocus(true)}
                         onBlur={() => setInputFocus(false)}
                         style={{ color: '#ffffff' }}
                         placeholder='Guardian Display Name'
-                        onChangeText={(guardianDisplayName: string) => setBungieId(guardianDisplayName)}
+                        onChangeText={(guardianDisplayName: string) => {
+                            setSelectedProfile('');
+                            setBungieId(guardianDisplayName);
+                        }}
+                        onSubmitEditing={(event) => setSelectedProfile(event.nativeEvent.text)}
                     />
-                    { !test ? <FlatList
-                        w='85%'
-                        data={userList}
-                        renderItem={({item}) => (
-                            <Pressable onPress={() => setTest('ALED')} backgroundColor='#ffffff' borderBottomWidth='1' _dark={{borderColor: 'coolGray.200'}} pl='4' pr='5' py='2'>
-                                {/*<HStack space={3} justifyContent='space-between'>*/}
-                                    <Text _dark={{color: 'warmGray.200'}} bold>
-                                        {item.bungieGlobalDisplayName + '#' + item.bungieGlobalDisplayNameCode}
-                                    </Text>
-                                {/*</HStack>*/}
-                            </Pressable>
-                            )
-                        }
-                        keyExtractor={(_, index) => index.toString()}
-                    /> : null }
                 </VStack>
             </View>
             {isError ?
