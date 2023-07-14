f608a6900b46e83317980b926629284d7720e5c2
Arthur <arthur.adam@epitech.eu>
2023-07-14 20:57:28 +0200

    Layout: Update HomeView layout and separate widgets

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -2,11 +2,14 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
-import 'package:drop_down_search_field/drop_down_search_field.dart';
 import 'package:get_it/get_it.dart';
 
+import 'package:guardian_dock/src/widgets/account_suggestion_tile.dart';
+import 'package:guardian_dock/src/widgets/empty_suggestion_tile.dart';
 import 'package:guardian_dock/api/models/bungie_account.dart';
+import 'package:guardian_dock/src/widgets/custom_appbar.dart';
 import 'package:guardian_dock/api/client_api.dart';
 
 class HomeView extends StatefulWidget {
@@ -45,22 +48,15 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        elevation: 2.0,
-        backgroundColor: Colors.transparent,
-        title: const Text(
-          "GuardianDock",
-          style: TextStyle(fontWeight: FontWeight.bold),
-          textAlign: TextAlign.start,
-        ),
-      ),
+      appBar: const GuardianDockAppbar(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            DropDownSearchField<String>(
+            TypeAheadField<BungieAccountData>(
+              hideOnEmpty: true,
               textFieldConfiguration: TextFieldConfiguration(
                 style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
                 decoration: InputDecoration(
@@ -74,41 +70,25 @@ class _HomeViewState extends State<HomeView> {
               ),
               suggestionsCallback: (String search) async {
                 lastFetchedAccounts = await getPossibleBungieAccounts(search);
-                return lastFetchedAccounts.map(
-                  (account) => "${account.bungieGlobalDisplayName}#${account.bungieGlobalDisplayNameCode}"
-                );
+                return lastFetchedAccounts;
               },
+              noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
               itemBuilder: (context, suggestion) {
-                // Since the combination "displayName#displayNameCode" is unique, we can use it to find the current account.
-                BungieAccountData relatedAccount = lastFetchedAccounts.firstWhere((element) => "${element.bungieGlobalDisplayName}#${element.bungieGlobalDisplayNameCode}" == suggestion);
-                if (relatedAccount.memberships!.isEmpty || relatedAccount.memberships!.indexWhere((element) => element.overrideType == element.membershipType) == -1) {
+                if (lastFetchedAccounts.isEmpty) {
                   return Container();
                 }
 
-                return ListTile(
-                  leading: Image.network(
-                    width: 40,
-                    height: 40,
-                    Uri.https(
-                      ApiClient.baseUrl,
-                      relatedAccount.memberships![
-                        relatedAccount.memberships!.indexWhere((element) => element.overrideType == element.membershipType)
-                      ].platformIconPath
-                    ).toString()
-                  ),
-                  title: Text(suggestion),
-                  tileColor: Theme.of(context).colorScheme.background,
-                  textColor: Theme.of(context).colorScheme.onBackground,
-                  onTap: () {
-                    _dropdownSearchFieldController.text = suggestion;
-                    // TODO
-                    // Do the redirection to the user's stats
-                  }
+                // Since the combination "displayName#displayNameCode" is unique, we can use it to find the current account.
+                BungieAccountData relatedAccount = lastFetchedAccounts.firstWhere(
+                  (e) => e.bungieNetMembershipId == suggestion.bungieNetMembershipId
                 );
+                if (relatedAccount.memberships!.isEmpty) {
+                  return Container();
+                }
+
+                return AccountSuggestionTile(relatedAccount: relatedAccount);
               },
-              onSuggestionSelected: (suggestion) {
-                _dropdownSearchFieldController.text = suggestion;
-              },
+              onSuggestionSelected: (suggestion) {},
             )
           ],
         ),
---
+++ lib/src/widgets/account_suggestion_tile.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'package:guardian_dock/api/models/destiny_membership.dart';
+import 'package:guardian_dock/api/models/bungie_account.dart';
+import 'package:guardian_dock/api/client_api.dart';
+
+class AccountSuggestionTile extends StatelessWidget {
+  const AccountSuggestionTile({required this.relatedAccount, super.key});
+
+  final BungieAccountData relatedAccount;
+
+  DestinyMembership getMainDestinyMembership() {
+    if (relatedAccount.isCrossSavedAccount) {
+      return relatedAccount.memberships!.singleWhere((element) => element.overrideType == element.membershipType);
+    }
+    return relatedAccount.memberships!.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.network(
+        width: 40,
+        height: 40,
+        Uri.https(
+          ApiClient.baseUrl,
+          getMainDestinyMembership().platformIconPath
+        ).toString()
+      ),
+      title: Text(
+        "${relatedAccount.bungieGlobalDisplayName}#${relatedAccount.bungieGlobalDisplayNameCode}"
+      ),
+      tileColor: Theme.of(context).colorScheme.background,
+      textColor: Theme.of(context).colorScheme.onBackground
+    );
+  }
+}
