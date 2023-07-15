d88c1b5bfba095986d6ec17aa72e1792d9d7da40
Arthur <arthur.adam@epitech.eu>
2023-07-15 16:55:37 +0200

    Tests: Adding BungieAccountData model test

--- test/widgets/empty_suggestion_test.dart
+++ test/widgets/empty_suggestion_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:guardian_dock/src/widgets/empty_suggestion_tile.dart';
 
 void main() {
