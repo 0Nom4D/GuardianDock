c699ad568ecc79bfa22f7d26050c66a65582f1a3
Arthur <arthur.adam@epitech.eu>
2023-07-15 02:42:24 +0200

    Layout: Updating Layout with Manifest Load / Fetch

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:get_it/get_it.dart';
@@ -25,6 +26,8 @@ class _HomeViewState extends State<HomeView> {
 
   List<BungieAccountData> lastFetchedAccounts = [];
 
+  Future<void> getManifest() async => await GetIt.I<ApiClient>().getManifest();
+
   Future<List<BungieAccountData>> getPossibleBungieAccounts(String bungieName) async {
     if (bungieName.isEmpty) {
       return [];
@@ -45,89 +48,108 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: const GuardianDockAppbar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height:  MediaQuery.of(context).size.height * .1),
-            Center(
-              child: RichText(
-                text: TextSpan(
-                  text: "Track",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.onBackground,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold
+    return FutureBuilder(
+      future: getManifest(),
+      builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CircularProgressIndicator(
+                color: Theme.of(context).colorScheme.tertiary
+              ),
+            )
+          );
+        }
+        return Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.background,
+          appBar: const GuardianDockAppbar(),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  SizedBox(height:  MediaQuery.of(context).size.height * .1),
+                  TypeAheadField<BungieAccountData>(
+                    hideOnEmpty: true,
+                    hideOnLoading: true,
+                    keepSuggestionsOnLoading: false,
+                    suggestionsBoxDecoration: SuggestionsBoxDecoration(
+                        color: Theme.of(context).colorScheme.background
+                    ),
+                    textFieldConfiguration: TextFieldConfiguration(
+                      style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+                      decoration: InputDecoration(
+                        filled: true,
+                        hintText: "Bungie ID",
+                        hintStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
+                        ),
+                      ),
+                      controller: _dropdownSearchFieldController,
+                    ),
+                    suggestionsCallback: (String search) async {
+                      lastFetchedAccounts = await getPossibleBungieAccounts(search);
+                      return lastFetchedAccounts;
+                    },
+                    noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
+                    itemBuilder: (context, suggestion) {
+                      if (suggestion.memberships!.isEmpty) {
+                        return Container();
+                      }
+
+                      return AccountSuggestionTile(relatedAccount: suggestion);
+                    },
+                    itemSeparatorBuilder: (context, index) => Divider(
+                        height: 1,
+                        color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
+                    ),
+                    onSuggestionSelected: (suggestion) {},
                   ),
-                  children: [
-                    TextSpan(
-                      text: " your light",
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.tertiary,
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold
+                  SizedBox(height:  MediaQuery.of(context).size.height * .1),
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Track",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold
+                        ),
+                        children: [
+                          TextSpan(
+                            text: " your light",
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.tertiary,
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold
+                            ),
+                          )
+                        ]
                       ),
                     )
-                  ]
-                ),
-              )
-            ),
-            SizedBox(height:  MediaQuery.of(context).size.height * .025),
-            Center(
-              child: Text(
-                "View all your statistics on the same platform.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onBackground,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500
-                ),
-              )
-            ),
-            SizedBox(height:  MediaQuery.of(context).size.height * .1),
-            TypeAheadField<BungieAccountData>(
-              hideOnEmpty: true,
-              keepSuggestionsOnLoading: false,
-              suggestionsBoxDecoration: SuggestionsBoxDecoration(
-                color: Theme.of(context).colorScheme.background
-              ),
-              textFieldConfiguration: TextFieldConfiguration(
-                style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-                decoration: InputDecoration(
-                  filled: true,
-                  hintText: "Bungie ID",
-                  hintStyle: TextStyle(
-                    color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
                   ),
-                ),
-                controller: _dropdownSearchFieldController,
+                  SizedBox(height:  MediaQuery.of(context).size.height * .025),
+                  Center(
+                    child: Text(
+                      "View all your statistics on the same platform.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500
+                      ),
+                    )
+                  )
+                ],
               ),
-              suggestionsCallback: (String search) async {
-                lastFetchedAccounts = await getPossibleBungieAccounts(search);
-                return lastFetchedAccounts;
-              },
-              noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
-              itemBuilder: (context, suggestion) {
-                if (suggestion.memberships!.isEmpty) {
-                  return Container();
-                }
-
-                return AccountSuggestionTile(relatedAccount: suggestion);
-              },
-              itemSeparatorBuilder: (context, index) => Divider(
-                height: 1,
-                color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
-              ),
-              onSuggestionSelected: (suggestion) {},
-            )
-          ],
-        ),
-      )
+            ),
+          )
+        );
+      },
     );
   }
 }
