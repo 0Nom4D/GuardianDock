61964dccaac381a35ebac2d46618fee4bc55ae44
Arthur <arthur.adam@epitech.eu>
2023-10-02 11:38:58 +0200

    RSS: Preparing RSS models JSON extraction

---
+++ lib/api/models/news_article.dart
@@ -0,0 +1,32 @@
+class NewsArticle {
+  String uid;
+  String title;
+  String link;
+  DateTime pubDate;
+  String description;
+  String htmlContent;
+  String bannerImagePath;
+  String optionalMobileAsset;
+
+  NewsArticle({
+    required this.uid,
+    required this.title,
+    required this.link,
+    required this.pubDate,
+    required this.description,
+    required this.htmlContent,
+    required this.bannerImagePath,
+    required this.optionalMobileAsset
+  });
+
+  NewsArticle.fromJson(Map<String, dynamic> json)
+      :
+        uid = json["UniqueIdentifier"],
+        title = json["Title"],
+        link = json["Link"],
+        pubDate = DateTime.parse(json["PubDate"]),
+        description = json["Description"],
+        htmlContent = json["HtmlContent"],
+        bannerImagePath = json["ImagePath"],
+        optionalMobileAsset = json["OptionalMobileImagePath"];
+}
