db71fc1b69bb3a5f4b07c24fbab2389524d47ce5
Arthur <arthur.adam@epitech.eu>
2023-10-04 00:31:40 +0200

    RSS: Updating HomeView layout with RSS Article fetch

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -9,8 +9,10 @@ import 'package:get_it/get_it.dart';
 
 import 'package:guardian_dock/src/widgets/account_suggestion_tile.dart';
 import 'package:guardian_dock/src/widgets/empty_suggestion_tile.dart';
+import 'package:guardian_dock/src/widgets/rss/rss_news_feed.dart';
 import 'package:guardian_dock/api/models/bungie_account.dart';
 import 'package:guardian_dock/src/widgets/custom_appbar.dart';
+import 'package:guardian_dock/api/models/news_article.dart';
 import 'package:guardian_dock/api/client_api.dart';
 
 class HomeView extends StatefulWidget {
@@ -24,6 +26,9 @@ class _HomeViewState extends State<HomeView> {
   final TextEditingController _dropdownSearchFieldController = TextEditingController();
 
   List<BungieAccountData> lastFetchedAccounts = [];
+  List<NewsArticle> fetchedArticles = [];
+
+  bool isLoadingNewsArticle = false;
 
   Future<void> getManifest() async => await GetIt.I<ApiClient>().getManifest();
 
@@ -47,14 +52,31 @@ class _HomeViewState extends State<HomeView> {
     }
   }
 
+  void getNewsArticles() async {
+    setState(() {
+      isLoadingNewsArticle = true;
+    });
+
+    fetchedArticles = await GetIt.I<ApiClient>().rss.getLatestArticles('fr');
+
+    setState(() {
+      isLoadingNewsArticle = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getNewsArticles();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: getManifest(),
       builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Align(
-            alignment: Alignment.bottomLeft,
+          return Center(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: CircularProgressIndicator(
@@ -64,89 +86,91 @@ class _HomeViewState extends State<HomeView> {
           );
         }
         return Scaffold(
+          resizeToAvoidBottomInset: false,
           backgroundColor: Theme.of(context).colorScheme.background,
           appBar: const GuardianDockAppbar(),
           body: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(height:  MediaQuery.of(context).size.height * .1),
-                  TypeAheadField<BungieAccountData>(
-                    hideOnEmpty: true,
-                    hideOnLoading: true,
-                    keepSuggestionsOnLoading: false,
-                    suggestionsBoxDecoration: SuggestionsBoxDecoration(
-                        color: Theme.of(context).colorScheme.background
-                    ),
-                    textFieldConfiguration: TextFieldConfiguration(
-                      style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-                      decoration: InputDecoration(
-                        filled: true,
-                        hintText: "Bungie ID",
-                        hintStyle: TextStyle(
-                            color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
-                        ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TypeAheadField<BungieAccountData>(
+                  hideOnEmpty: true,
+                  hideOnLoading: true,
+                  keepSuggestionsOnLoading: false,
+                  suggestionsBoxDecoration: SuggestionsBoxDecoration(
+                      color: Theme.of(context).colorScheme.background
+                  ),
+                  textFieldConfiguration: TextFieldConfiguration(
+                    style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+                    decoration: InputDecoration(
+                      filled: true,
+                      hintText: "Bungie ID",
+                      hintStyle: TextStyle(
+                          color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
                       ),
-                      controller: _dropdownSearchFieldController,
                     ),
-                    suggestionsCallback: (String search) async {
-                      lastFetchedAccounts = await getPossibleBungieAccounts(search);
-                      return lastFetchedAccounts;
-                    },
-                    noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
-                    itemBuilder: (context, suggestion) {
-                      if (suggestion.memberships!.isEmpty) {
-                        return Container();
-                      }
+                    controller: _dropdownSearchFieldController,
+                  ),
+                  suggestionsCallback: (String search) async {
+                    lastFetchedAccounts = await getPossibleBungieAccounts(search);
+                    return lastFetchedAccounts;
+                  },
+                  noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
+                  itemBuilder: (context, suggestion) {
+                    if (suggestion.memberships!.isEmpty) {
+                      return Container();
+                    }
 
-                      return AccountSuggestionTile(relatedAccount: suggestion);
-                    },
-                    itemSeparatorBuilder: (context, index) => Divider(
-                        height: 1,
-                        color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
-                    ),
-                    onSuggestionSelected: (suggestion) {},
+                    return AccountSuggestionTile(relatedAccount: suggestion);
+                  },
+                  itemSeparatorBuilder: (context, index) => Divider(
+                      height: 1,
+                      color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
                   ),
-                  SizedBox(height:  MediaQuery.of(context).size.height * .1),
-                  Center(
-                    child: RichText(
-                      text: TextSpan(
-                        text: "Track",
-                        style: TextStyle(
-                          color: Theme.of(context).colorScheme.onBackground,
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold
-                        ),
-                        children: [
-                          TextSpan(
-                            text: " your light",
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.tertiary,
-                              fontSize: 35,
-                              fontWeight: FontWeight.bold
-                            ),
-                          )
-                        ]
-                      ),
-                    )
-                  ),
-                  SizedBox(height:  MediaQuery.of(context).size.height * .025),
-                  Center(
-                    child: Text(
-                      "View all your statistics on the same platform.",
-                      textAlign: TextAlign.center,
+                  onSuggestionSelected: (suggestion) {},
+                ),
+                Center(
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Track",
                       style: TextStyle(
                         color: Theme.of(context).colorScheme.onBackground,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold
                       ),
-                    )
+                      children: [
+                        TextSpan(
+                          text: " your light",
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.tertiary,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ]
+                    ),
                   )
-                ],
-              ),
+                ),
+                Center(
+                  child: Text(
+                    "View all your statistics on the same platform.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500
+                    ),
+                  )
+                ),
+                !isLoadingNewsArticle ? SizedBox(
+                  height: MediaQuery.of(context).size.height * .4,
+                  child: NewsArticlesFeedList(rssFeed: fetchedArticles)
+                ) : const Center(
+                  child: CircularProgressIndicator()
+                )
+              ],
             ),
           )
         );
