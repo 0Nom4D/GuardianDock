d70aea6642e9f45a325a471710d8459c041cc7b3
Arthur <arthur.adam@epitech.eu>
2023-10-05 14:37:44 +0200

    Slivers: Improving Persistent Search bar quality to SliverAppBar for elevation

--- lib/src/widgets/persistent_search_bar.dart
+++ lib/src/widgets/persistent_search_bar.dart
@@ -12,19 +12,17 @@ import 'package:guardian_dock/src/widgets/empty_suggestion_tile.dart';
 import 'package:guardian_dock/api/models/bungie_account.dart';
 import 'package:guardian_dock/api/client_api.dart';
 
-class PersistentSearchBarDelegate extends SliverPersistentHeaderDelegate {
+class PersistentSearchBar extends StatefulWidget {
+  const PersistentSearchBar({super.key});
+
+  @override
+  State<PersistentSearchBar> createState() => _PersistentSearchBarState();
+}
+
+class _PersistentSearchBarState extends State<PersistentSearchBar> {
   final TextEditingController _dropdownSearchFieldController = TextEditingController();
 
   List<BungieAccountData> lastFetchedAccounts = [];
-
-  final double maxSearchBarExtent;
-  final double minSearchBarExtent;
-
-  PersistentSearchBarDelegate({
-    required this.maxSearchBarExtent,
-    required this.minSearchBarExtent,
-    Key? key
-  });
 
   Future<List<BungieAccountData>> getPossibleBungieAccounts(String bungieName) async {
     if (bungieName.isEmpty) {
@@ -35,9 +33,9 @@ class PersistentSearchBarDelegate extends SliverPersistentHeaderDelegate {
 
     try {
       return await GetIt
-          .I<ApiClient>()
-          .search
-          .searchByBungieID(bungieName);
+        .I<ApiClient>()
+        .search
+        .searchByBungieID(bungieName);
     } on HttpException catch (ex) {
       if (kDebugMode) {
         log(ex.message);
@@ -47,73 +45,46 @@ class PersistentSearchBarDelegate extends SliverPersistentHeaderDelegate {
   }
 
   @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return TypeAheadField<BungieAccountData>(
-      hideOnEmpty: true,
-      hideOnLoading: true,
-      keepSuggestionsOnLoading: false,
-      suggestionsBoxDecoration: SuggestionsBoxDecoration(
-        color: Theme.of(context).colorScheme.background
-      ),
-      textFieldConfiguration: TextFieldConfiguration(
-        style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-        decoration: InputDecoration(
-          filled: true,
-          hintText: "Bungie ID",
-          hintStyle: TextStyle(
-            color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
-          ),
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: TypeAheadField<BungieAccountData>(
+        hideOnEmpty: true,
+        hideOnLoading: true,
+        minCharsForSuggestions: 2,
+        keepSuggestionsOnLoading: false,
+        suggestionsBoxDecoration: SuggestionsBoxDecoration(
+          color: Theme.of(context).colorScheme.background
         ),
-        controller: _dropdownSearchFieldController,
-      ),
-      suggestionsCallback: (String search) async {
-        lastFetchedAccounts = await getPossibleBungieAccounts(search);
-        return lastFetchedAccounts;
-      },
-      noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
-      itemBuilder: (context, suggestion) {
-        if (suggestion.memberships!.isEmpty) {
-          return Container();
-        }
-
-        return AccountSuggestionTile(relatedAccount: suggestion);
-      },
-      itemSeparatorBuilder: (context, index) => Divider(
+        textFieldConfiguration: TextFieldConfiguration(
+          style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+          decoration: InputDecoration(
+            filled: true,
+            hintText: "Bungie ID",
+            fillColor: Theme.of(context).colorScheme.background,
+            hintStyle: TextStyle(
+              color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
+            ),
+          ),
+          controller: _dropdownSearchFieldController,
+        ),
+        suggestionsCallback: (String search) async {
+          lastFetchedAccounts = await getPossibleBungieAccounts(search);
+          return lastFetchedAccounts;
+        },
+        noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
+        itemBuilder: (context, suggestion) {
+          if (suggestion.memberships!.isEmpty) {
+            return Container();
+          }
+          return AccountSuggestionTile(relatedAccount: suggestion);
+        },
+        itemSeparatorBuilder: (context, index) => Divider(
           height: 1,
           color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
+        ),
+        onSuggestionSelected: (suggestion) {},
       ),
-      onSuggestionSelected: (suggestion) {},
-    );
-  }
-
-  @override
-  double get maxExtent => maxSearchBarExtent;
-
-  @override
-  double get minExtent => minSearchBarExtent;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) => true;
-}
-
-class PersistentSearchBar extends StatelessWidget {
-  final double maxExtent;
-  final double minExtent;
-
-  const PersistentSearchBar({
-    required this.maxExtent,
-    required this.minExtent,
-    super.key
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverPersistentHeader(
-      pinned: true,
-      delegate: PersistentSearchBarDelegate(
-        maxSearchBarExtent: maxExtent,
-        minSearchBarExtent: minExtent
-      )
     );
   }
 }
