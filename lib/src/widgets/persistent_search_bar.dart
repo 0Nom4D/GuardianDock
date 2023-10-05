3c38c41e2cdf2832a7ae90764077bb2f31e9d389
Arthur <arthur.adam@epitech.eu>
2023-10-05 14:46:18 +0200

    Slivers: Applying symmetric padding on horizontal axis for SliverAppBar

--- lib/src/widgets/persistent_search_bar.dart
+++ lib/src/widgets/persistent_search_bar.dart
@@ -46,44 +46,47 @@ class _PersistentSearchBarState extends State<PersistentSearchBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: TypeAheadField<BungieAccountData>(
-        hideOnEmpty: true,
-        hideOnLoading: true,
-        minCharsForSuggestions: 2,
-        keepSuggestionsOnLoading: false,
-        suggestionsBoxDecoration: SuggestionsBoxDecoration(
-          color: Theme.of(context).colorScheme.background
-        ),
-        textFieldConfiguration: TextFieldConfiguration(
-          style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-          decoration: InputDecoration(
-            filled: true,
-            hintText: "Bungie ID",
-            fillColor: Theme.of(context).colorScheme.background,
-            hintStyle: TextStyle(
-              color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
-            ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Align(
+        alignment: Alignment.center,
+        child: TypeAheadField<BungieAccountData>(
+          hideOnEmpty: true,
+          hideOnLoading: true,
+          minCharsForSuggestions: 2,
+          keepSuggestionsOnLoading: false,
+          suggestionsBoxDecoration: SuggestionsBoxDecoration(
+            color: Theme.of(context).colorScheme.background
           ),
-          controller: _dropdownSearchFieldController,
+          textFieldConfiguration: TextFieldConfiguration(
+            style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+            decoration: InputDecoration(
+              filled: true,
+              hintText: "Bungie ID",
+              fillColor: Theme.of(context).colorScheme.background,
+              hintStyle: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
+              ),
+            ),
+            controller: _dropdownSearchFieldController,
+          ),
+          suggestionsCallback: (String search) async {
+            lastFetchedAccounts = await getPossibleBungieAccounts(search);
+            return lastFetchedAccounts;
+          },
+          noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
+          itemBuilder: (context, suggestion) {
+            if (suggestion.memberships!.isEmpty) {
+              return Container();
+            }
+            return AccountSuggestionTile(relatedAccount: suggestion);
+          },
+          itemSeparatorBuilder: (context, index) => Divider(
+            height: 1,
+            color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
+          ),
+          onSuggestionSelected: (suggestion) {},
         ),
-        suggestionsCallback: (String search) async {
-          lastFetchedAccounts = await getPossibleBungieAccounts(search);
-          return lastFetchedAccounts;
-        },
-        noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
-        itemBuilder: (context, suggestion) {
-          if (suggestion.memberships!.isEmpty) {
-            return Container();
-          }
-          return AccountSuggestionTile(relatedAccount: suggestion);
-        },
-        itemSeparatorBuilder: (context, index) => Divider(
-          height: 1,
-          color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
-        ),
-        onSuggestionSelected: (suggestion) {},
       ),
     );
   }
