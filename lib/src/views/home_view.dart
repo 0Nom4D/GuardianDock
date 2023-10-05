6cf61361ded62df2440343335936e8af82ebd7be
Arthur <arthur.adam@epitech.eu>
2023-10-05 14:58:37 +0200

    Slivers: Implementing Slivers in HomeView

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,16 +1,9 @@
-import 'dart:developer';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:get_it/get_it.dart';
 
-import 'package:guardian_dock/src/widgets/account_suggestion_tile.dart';
-import 'package:guardian_dock/src/widgets/empty_suggestion_tile.dart';
+import 'package:guardian_dock/src/widgets/persistent_search_bar.dart';
 import 'package:guardian_dock/src/widgets/rss/rss_news_feed.dart';
-import 'package:guardian_dock/api/models/bungie_account.dart';
 import 'package:guardian_dock/src/widgets/custom_appbar.dart';
 import 'package:guardian_dock/api/models/news_article.dart';
 import 'package:guardian_dock/api/client_api.dart';
@@ -23,34 +16,11 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  final TextEditingController _dropdownSearchFieldController = TextEditingController();
-
-  List<BungieAccountData> lastFetchedAccounts = [];
   List<NewsArticle> fetchedArticles = [];
 
   bool isLoadingNewsArticle = false;
 
   Future<void> getManifest() async => await GetIt.I<ApiClient>().getManifest();
-
-  Future<List<BungieAccountData>> getPossibleBungieAccounts(String bungieName) async {
-    if (bungieName.isEmpty) {
-      return [];
-    } else if (bungieName.contains('#')) {
-      bungieName = bungieName.split('#')[0];
-    }
-
-    try {
-      return await GetIt
-        .I<ApiClient>()
-        .search
-        .searchByBungieID(bungieName);
-    } on HttpException catch (ex) {
-      if (kDebugMode) {
-        log(ex.message);
-      }
-      return [];
-    }
-  }
 
   void getNewsArticles() async {
     setState(() {
@@ -89,50 +59,21 @@ class _HomeViewState extends State<HomeView> {
           resizeToAvoidBottomInset: false,
           backgroundColor: Theme.of(context).colorScheme.background,
           appBar: const GuardianDockAppbar(),
-          body: Padding(
-            padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TypeAheadField<BungieAccountData>(
-                  hideOnEmpty: true,
-                  hideOnLoading: true,
-                  keepSuggestionsOnLoading: false,
-                  suggestionsBoxDecoration: SuggestionsBoxDecoration(
-                    color: Theme.of(context).colorScheme.background
-                  ),
-                  textFieldConfiguration: TextFieldConfiguration(
-                    style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
-                    decoration: InputDecoration(
-                      filled: true,
-                      hintText: "Bungie ID",
-                      hintStyle: TextStyle(
-                        color: Theme.of(context).colorScheme.onBackground.withOpacity(.3)
-                      ),
-                    ),
-                    controller: _dropdownSearchFieldController,
-                  ),
-                  suggestionsCallback: (String search) async {
-                    lastFetchedAccounts = await getPossibleBungieAccounts(search);
-                    return lastFetchedAccounts;
-                  },
-                  noItemsFoundBuilder: (context) => const EmptySuggestionTile(),
-                  itemBuilder: (context, suggestion) {
-                    if (suggestion.memberships!.isEmpty) {
-                      return Container();
-                    }
-
-                    return AccountSuggestionTile(relatedAccount: suggestion);
-                  },
-                  itemSeparatorBuilder: (context, index) => Divider(
-                      height: 1,
-                      color: Theme.of(context).colorScheme.onBackground.withOpacity(.15)
-                  ),
-                  onSuggestionSelected: (suggestion) {},
-                ),
-                SizedBox(height: MediaQuery.of(context).size.height *.05),
-                Center(
+          body: CustomScrollView(
+            shrinkWrap: true,
+            slivers: [
+              SliverAppBar(
+                backgroundColor: Theme.of(context).colorScheme.background,
+                toolbarHeight: MediaQuery.of(context).size.height * .05,
+                collapsedHeight: MediaQuery.of(context).size.height * .05,
+                expandedHeight: MediaQuery.of(context).size.height * .1,
+                pinned: true,
+                elevation: 20,
+                centerTitle: true,
+                flexibleSpace: const PersistentSearchBar()
+              ),
+              SliverToBoxAdapter(
+                child: Center(
                   child: RichText(
                     text: TextSpan(
                       text: "Track",
@@ -154,8 +95,12 @@ class _HomeViewState extends State<HomeView> {
                     ),
                   )
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height *.025),
-                Center(
+              ),
+              SliverToBoxAdapter(
+                child: SizedBox(height: MediaQuery.of(context).size.height *.025)
+              ),
+              SliverToBoxAdapter(
+                child: Center(
                   child: Text(
                     "View all your statistics on the same platform.",
                     textAlign: TextAlign.center,
@@ -166,15 +111,18 @@ class _HomeViewState extends State<HomeView> {
                     ),
                   )
                 ),
-                SizedBox(height: MediaQuery.of(context).size.height * .05),
-                !isLoadingNewsArticle ? Expanded(
-                  flex: 2,
-                  child: NewsArticlesFeedList(rssFeed: fetchedArticles)
-                ) : const Center(
-                  child: CircularProgressIndicator()
-                )
-              ],
-            ),
+              ),
+              SliverToBoxAdapter(
+                child: SizedBox(height: MediaQuery.of(context).size.height *.05)
+              ),
+              !isLoadingNewsArticle
+                ? NewsArticlesFeedList(rssFeed: fetchedArticles)
+                : const SliverToBoxAdapter(
+                  child: Center(
+                    child: CircularProgressIndicator()
+                  ),
+                ),
+            ],
           )
         );
       },
