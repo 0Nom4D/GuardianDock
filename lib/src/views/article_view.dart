85e6dd2405e7702acce64bfedf9fe6303aed4e26
Arthur <arthur.adam@epitech.eu>
2023-10-20 21:37:37 +0200

    RSS: Implementing first version of Article Layout

--- lib/src/views/article_view.dart
+++ lib/src/views/article_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+
+import 'package:guardian_dock/src/widgets/rss/news_article_header.dart';
+import 'package:guardian_dock/src/widgets/custom_appbar.dart';
 import 'package:guardian_dock/api/models/news_article.dart';
 
 class ArticleView extends StatelessWidget {
@@ -9,6 +13,35 @@ class ArticleView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: const GuardianDockAppbar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: NewsArticleHeader(article: article),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: HtmlWidget(
+                article.htmlContent,
+                enableCaching: true,
+                customStylesBuilder: (element) {
+                  if (element.outerHtml.startsWith("<a href")) {
+                    return { 'color': "#${Theme.of(context).colorScheme.tertiary.value.toRadixString(16).substring(2)}" };
+                  }
+
+                  return null;
+                },
+                textStyle: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+              ),
+            ),
+          ),
+        ]
+      )
+    );
   }
 }
