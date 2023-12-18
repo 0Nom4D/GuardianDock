d0015e99ce1aaa24fa24ec8dcd9fdcf6e436d1d0
Arthur <arthur.adam@epitech.eu>
2023-12-18 13:42:28 +0100

    API: Handling pagination inside API wrapper

--- lib/api/rss.dart
+++ lib/api/rss.dart
@@ -9,10 +9,13 @@ import 'package:guardian_dock/api/models/news_article.dart';
 class Rss {
   final ApiClient _client;
 
-  const Rss(ApiClient client) : _client = client;
+  bool isMoreToFetch = true;
+  int currentPage = 0;
 
-  Future<List<NewsArticle>> getLatestArticles(String locale, {int pagination = 0}) async {
-    final path = Uri.https(ApiClient.baseUrl, "Platform/Content/Rss/NewsArticles/$pagination", {"includebody": "true", "lc": locale});
+  Rss(ApiClient client) : _client = client;
+
+  Future<List<NewsArticle>> getLatestArticles(String locale) async {
+    final path = Uri.https(ApiClient.baseUrl, "Platform/Content/Rss/NewsArticles/$currentPage", {"includebody": "true", "lc": locale});
     final response = await _client.client.get(path, headers: _client.headers);
 
     if (response.statusCode == 503) {
@@ -20,6 +23,11 @@ class Rss {
     } else if (response.statusCode >= 400) {
       throw HttpException(response.body);
     }
+    if (jsonDecode(utf8.decode(response.bodyBytes))['Response']['NewsArticles'] == null) {
+      isMoreToFetch = false;
+      return [];
+    }
+    currentPage++;
     return List<NewsArticle>.from(jsonDecode(utf8.decode(response.bodyBytes))['Response']['NewsArticles'].map((account) => NewsArticle.fromJson(account)));
   }
 }
